e5266a2c89e72f7b27ddd8809a41473aad1f1e85
DK\DTAT <dtat@sitecore.net>
2017-12-07 15:01:56 +0200

    Initial code

---
+++ src/Sitecore.Support.197107/Marketing/xMgmt/Definitions/ItemEventHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace Sitecore.Support.Marketing.xMgmt.Definitions
+{
+    using Sitecore.Abstractions;
+    using System;
+    using System.Reflection;
+
+    internal class ItemEventHandler : Sitecore.Marketing.xMgmt.Definitions.ItemEventHandler
+    {
+        private BaseTemplateManager _templateManager;
+
+        public ItemEventHandler()
+        {
+            _templateManager = GetBaseTemplateManager();
+        }
+
+        public void SupportOnItemSaving([NotNull] object sender, [CanBeNull] EventArgs args)
+        {
+            OnItemSaving(sender, args);
+        }
+
+        public void SupportOnItemCreating(object sender, EventArgs args)
+        {
+            OnItemCreating(sender, args);
+        }
+
+        #region Supplementary code
+
+        private BaseTemplateManager GetBaseTemplateManager()
+        {
+            Assembly assembly = Assembly.Load("Sitecore.Marketing.xMgmt");
+            Type type = assembly.GetType("Sitecore.Marketing.xMgmt.Definitions.ItemEventHandler");
+            Sitecore.Marketing.xMgmt.Definitions.ItemEventHandler obj = (Sitecore.Marketing.xMgmt.Definitions.ItemEventHandler)Activator.CreateInstance(type);
+            FieldInfo info = obj.GetType().GetField("_templateManager", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            return (BaseTemplateManager)info.GetValue(this);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Sitecore.Support.197107/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.197107")]
+[assembly: AssemblyProduct("Sitecore.Support.197107")]
+[assembly: ComVisible(false)]
